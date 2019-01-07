16c8f69797478dcb3d99c2186580d35a65604c89
Michał <michal.rojek.94@gmail.com>
2019-01-07 23:01:47 +0100

    Added updating and deleting api for events

--- routes/events.js
+++ routes/events.js
@@ -52,6 +52,55 @@ router.post('/create', ensureModerator, (req, res) => {
     }
 });
 
+router.put('/update/:id', ensureModerator, (req, res) => {
+    req.checkBody('title', 'Event title is required.').notEmpty();
+    req.checkBody('body', 'Event description is required.').notEmpty();
+    req.checkBody('address_id', 'Event address is required.').notEmpty();
+    req.checkBody('date', 'Event date is required.').notEmpty();
+    req.checkBody('date', 'Event date should be added in correct format.').isISO8601();
+
+    let errors = req.validationErrors();
+
+    if(errors) {
+        res.status(200).json({
+            errors
+        });
+    } else {
+        const event = {
+            title: req.body.title,
+            body: req.body.body,
+            address_id: req.body.address_id,
+            date: req.body.date
+        }
+
+        Event.where('id', req.params.id).fetch().then(function(result) {
+            result.set(event).save().then(function(model) {
+                res.status(200).json({
+                    message: "Updated event."
+                });
+            }, function(err) {
+                res.status(400).json({
+                    message: err
+                });
+            });
+        })
+    }
+});
+
+router.delete('/id/:id', ensureModerator, function(req, res) {
+    new Event({id: req.params.id})
+    .destroy()
+    .then(function(model) {
+        res.status(200).json({
+            message: "Deleted event."
+        });
+    }, function(err) {
+        res.status(400).json({
+            message: err
+        });
+    });
+});
+
 function ensureAuthenticated(req, res, next) {
     if(typeof res.locals.userRole !== "undefined" && (res.locals.userRole === 1 || res.locals.userRole === 2 || res.locals.userRole === 3)) {
         return next();
